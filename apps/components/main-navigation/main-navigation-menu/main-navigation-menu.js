f729d4281145d670a1da73bf1865d0b8f91520ce
MoyseysPecaai <moyseys.veroni@pecaai.com.br>
2025-02-18 15:55:00 -0300

    refactor(reports): refatora layout e main-navigation

--- apps/components/main-navigation/main-navigation-menu/main-navigation-menu.js
+++ apps/components/main-navigation/main-navigation-menu/main-navigation-menu.js
@@ -20,17 +20,18 @@ document.addEventListener("DOMContentLoaded", function () {
     function renderMenu() {
         const container = document.getElementById("menuContainer");
         if (!container) return;
-        container.innerHTML = '';
+        container.innerHTML = "";
 
         const ulGroups = document.createElement("ul");
         ulGroups.classList.add("main-navigation-menu__groups");
 
-        groups.forEach(group => {
+        groups.forEach((group) => {
             const liGroup = document.createElement("li");
             const h4 = document.createElement("h4");
             h4.classList.add("main-navigation-menu__groups__name");
+            h4.setAttribute("id", "menu-group-name");
             if (expanded) {
-                h4.classList.add("--is-expanded");
+                h4.classList.add("is-expanded");
             }
             h4.textContent = group.name;
             liGroup.appendChild(h4);
@@ -38,14 +39,15 @@ document.addEventListener("DOMContentLoaded", function () {
             const ulModules = document.createElement("ul");
             ulModules.classList.add("main-navigation-menu__modules");
 
-            group.modules.forEach(module => {
+            group.modules.forEach((module) => {
                 const liModule = document.createElement("li");
                 liModule.classList.add("main-navigation-menu__item");
 
                 const a = document.createElement("a");
                 a.classList.add("main-navigation-menu__link");
+                a.setAttribute("id", "menu-navigation-link")
                 if (expanded) {
-                    a.classList.add("--is-expanded");
+                    a.classList.add("is-expanded");
                 }
                 a.href = `/${module.path}`;
                 a.addEventListener("click", function () {
@@ -61,6 +63,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 const divLabel = document.createElement("div");
                 divLabel.classList.add("main-navigation-menu__link__label");
+                divLabel.setAttribute("id", "menu-link-label");
                 const spanLabel = document.createElement("span");
                 spanLabel.textContent = module.label;
                 divLabel.appendChild(spanLabel);
@@ -81,19 +84,25 @@ document.addEventListener("DOMContentLoaded", function () {
     function toggleMenuExpansion() {
         expanded = !expanded;
         renderMenu();
+        console.log(expanded)
 
         const toggleBtn = document.getElementById("menuToggleBtn");
         if (toggleBtn) {
-            toggleBtn.classList.toggle("--is-expanded", expanded);
+            toggleBtn.classList.toggle("is-expanded", expanded);
             toggleBtn.querySelector(".icon").textContent = expanded
                 ? "keyboard_double_arrow_left"
                 : "keyboard_double_arrow_right";
         }
 
         const menu = document.getElementById("mainNavMenu");
-        if (menu) {
-            menu.classList.toggle("menu-opened", expanded);
-        }
+        const labels = document.querySelectorAll("#menu-link-label")
+        const menuGroupNames = document.querySelectorAll("#menu-group-name")
+        const menuNavigationLinks = document.querySelectorAll("#menu-navigation-link")
+
+        if(expanded && labels) labels.forEach((label) => label.classList.toggle("hide-label", expanded))
+        if(expanded && menuGroupNames) menuGroupNames.forEach((menuGroupName) => menuGroupName.classList.toggle("hide-menu-group-name", expanded))
+        if (!expanded && menu) menu.classList.toggle("menu-opened", expanded);
+        if(expanded && menuNavigationLinks) menuNavigationLinks.forEach((menuNavigationLink) => menuNavigationLink.classList.toggle("hide-menu-navigation-link", expanded))
     }
 
     function onClickMenuItem() {
